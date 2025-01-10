29433cd1367fec9ed1aa1f213de3c07787da04fa
0xOzgur <29779769+0xOzgur@users.noreply.github.com>
2025-01-10 21:56:48 +0300

    denemeeee

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,9 +1,17 @@
 module.exports = {
-    apps: [{
-      name: "ozguruzden-server",
-      script: "server/server.js",
-      env_production: {
-        NODE_ENV: "production"
-      }
-    }]
-  }
+  apps : [{
+    name: "ozguruzden-server",
+    script: "/var/www/ozguruzden.com/server/server.js",
+    env_production: {
+      NODE_ENV: "production"
+    },
+    instances: 1,
+    exec_mode: "fork",
+    max_memory_restart: '1G',
+    autorestart: true,
+    watch: false,
+    max_restarts: 3,
+    kill_timeout: 3000,
+    min_uptime: '60s'
+  }]
+};
