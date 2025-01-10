49843cb0f9eb252a5c47cba42013e1b5370734fa
0xOzgur <29779769+0xOzgur@users.noreply.github.com>
2025-01-10 17:43:21 +0300

    api update

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -19,7 +19,7 @@ function Contact() {
     setSuccess(null);
 
     try {
-      const response = await fetch('http://localhost:3001/api/send-email', {
+      const response = await fetch('http://localhost:3001/send-email', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
