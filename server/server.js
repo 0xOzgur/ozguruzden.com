c2386823dff9af744569b9dd0848d49d9eff3145
0xOzgur <29779769+0xOzgur@users.noreply.github.com>
2025-01-10 21:17:18 +0300

    server update

--- server/server.js
+++ server/server.js
@@ -24,6 +24,16 @@ app.use((req, res, next) => {
     next();
 });
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://ozguruzden.com');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Accept');
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(200);
+    }
+    next();
+});
+
 // Test endpoint
 app.get('/api', (req, res) => {
     console.log('Test endpoint hit!');
