3e08a130da1e53611f26f041641f7575b6c772f7
0xOzgur <29779769+0xOzgur@users.noreply.github.com>
2025-01-11 20:14:21 +0300

    certificate update

--- server/server.js
+++ server/server.js
@@ -95,8 +95,8 @@ const PORT = process.env.PORT || 3001;
 
 // HTTPS configuration
 const options = {
-    key: fs.readFileSync('/etc/letsencrypt/live/ozguruzden.com/privkey.pem'),
-    cert: fs.readFileSync('/etc/letsencrypt/live/ozguruzden.com/fullchain.pem')
+    key: fs.readFileSync('/etc/ssl//ozguruzden.com/privkey.key'),
+    cert: fs.readFileSync('/etc/ssl//ozguruzden.com/fullchain.crt')
 };
 
 https.createServer(options, app).listen(PORT, '0.0.0.0', () => {
