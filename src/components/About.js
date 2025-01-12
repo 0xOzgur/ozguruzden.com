d1aae8cbbe22443fea6a2df056cddc8a4718daea
0xOzgur <29779769+0xOzgur@users.noreply.github.com>
2025-01-12 09:55:07 +0300

    update

--- src/components/About.js
+++ src/components/About.js
@@ -11,7 +11,7 @@ function About() {
         { name: 'Solidity', level: 85, icon: 'fab fa-bitcoin' },
         { name: 'MySQL/MongoDB', level: 80, icon: 'fab fa-node-js' },
         { name: 'Git', level: 90, icon: 'fab fa-git-alt' },
-      ];
+    ];
 
     const interests = [
         { name: 'Web Development', icon: 'fas fa-code' },
@@ -20,91 +20,92 @@ function About() {
         { name: 'Playing Saxophone', icon: 'fas fa-music' },
     ];
 
+    const handleClick = (e) => {
+        e.preventDefault();
+        const targetId = e.currentTarget.getAttribute('href').slice(1);
+        const targetElement = document.getElementById(targetId);
+
+        if (targetElement) {
+            targetElement.scrollIntoView({
+                behavior: 'smooth',
+                block: 'start',
+            });
+        }
+    };
+
     return (
         <section id="about" className="about-section">
             <div className="about-container">
                 <h2 className="about-title">About Me</h2>
                 
                 <div className="about-content">
-                <div className="about-image-container">
-                    <img 
-                        src="https://i1.rgstatic.net/ii/profile.image/864925052317704-1583225610421_Q512/Ozgur-Uzden.jpg" 
-                        alt="Özgür" 
-                        className="about-image"
-                    />
-                <div className="image-overlay"></div>
-                <div className="image-border"></div>
-                <div className="interests-section areas-of-interest">
-                    <h3 className="card-title" class>Areas of Interest</h3>
-                    {interests.map((interest, index) => (
-                    <div key={index} className="skill-item">
-                        <i className={`fa ${interest.icon} interest-icon`} />
-                        <span>{interest.name}</span>
-                    </div>
-                    ))}
-                </div>
+                    <div className="left-column">
+                        <div className="about-image-container">
+                            <img 
+                                src="https://i1.rgstatic.net/ii/profile.image/864925052317704-1583225610421_Q512/Ozgur-Uzden.jpg" 
+                                alt="Özgür" 
+                                className="about-image"
+                            />
+                            <div className="image-overlay"></div>
+                            <div className="image-border"></div>
+                        </div>
 
+                        <div className="interests-section desktop-interests">
+                            <h3 className="card-title">Personal Interests</h3>
+                            <div className="interests-grid">
+                                {interests.map((interest, index) => (
+                                    <div key={index} className="interest-item">
+                                        <i className={`fa ${interest.icon} interest-icon`} />
+                                        <span className="interest-name">{interest.name}</span>
+                                    </div>
+                                ))}
+                            </div>
+                        </div>
                     </div>
                     
                     <div className="about-text-content">
-                    <div className="introduction">
-                        
-                        <p className="lead-text">
-                            It's Özgür, a seasoned web developer, engineer, and entrepreneur.
-                        </p>
+                        <div className="introduction">
+                            <p className="lead-text">
+                                It's Özgür, a seasoned web developer, engineer, and entrepreneur.
+                            </p>
                         </div>
                         
                         <div className="main-content">
-                        <p>
-    He's also the producer of <a href='https://www.orfion.com.tr' className="custom-link" target='_blank' rel="noopener noreferrer">Orfion Olive Oil</a>, a testament to his commitment to quality. 
-</p>
-                            
                             <p>
-                            This dedication seamlessly translates into his web development work, where he crafts beautiful, functional websites with meticulous attention to detail.  
+                                I produce <a href='https://www.orfion.com.tr' className="custom-link" target='_blank' rel="noopener noreferrer">Orfion Olive Oil</a> which shows how much I care about quality.
                             </p>
                             
                             <p>
-                            Beyond the digital and the olive grove, Özgür is a fan of Blockchain technologies, a devoted dad for his doughter, enjoys photography and playing the saxophone. He brings a unique blend of technical prowess and creative passion to every project.
+                                This same dedication is what I bring to my web development projects. I love creating websites that are not only beautiful but also work really well.
+                            </p>
+                            
+                            <p>
+                                Outside of work and olive oil, I'm big fan of Blockchain technologies. I'm also a proud dad to my daughter. In my free time, I enjoy taking photos and playing the saxophone. I mix my technical skills with my creative side in every project I work on.
                             </p>
                         </div>
                         
                         <div className="skills-section">
-  <h3 className="card-title">Technical Skills</h3>
-  <div className="two-column-grid">
-    {skills.slice(0, 5).map((skill, index) => (
-      <div key={index} className="skill-item">
-        <i className={`fa ${skill.icon} skill-icon`} />
-        <span className="skill-name">{skill.name}</span>
-        <div className="skill-bar-container">
-          <div 
-            className="skill-bar" 
-            style={{ width: `${skill.level}%` }}
-          >
-            <span className="skill-level">{skill.level}%</span>
-          </div>
-        </div>
-      </div>
-    ))}
-    {skills.slice(5).map((skill, index) => (
-      <div key={index + 5} className="skill-item">
-        <i className={`fa ${skill.icon} skill-icon`} />
-        <span className="skill-name">{skill.name}</span>
-        <div className="skill-bar-container">
-          <div 
-            className="skill-bar" 
-            style={{ width: `${skill.level}%` }}
-          >
-            <span className="skill-level">{skill.level}%</span>
-          </div>
-        </div>
-      </div>
-    ))}
-  </div>
-</div>
+                            <h3 className="card-title">Technical Skills</h3>
+                            <div className="two-column-grid">
+                                {skills.map((skill, index) => (
+                                    <div key={index} className="skill-item">
+                                        <i className={`fa ${skill.icon} skill-icon`} />
+                                        <span className="skill-name">{skill.name}</span>
+                                        <div className="skill-bar-container">
+                                            <div 
+                                                className="skill-bar" 
+                                                style={{ width: `${skill.level}%` }}
+                                            >
+                                                <span className="skill-level">{skill.level}%</span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                ))}
+                            </div>
+                        </div>
                         
                         <div className="cta-section">
-                            <p>To learn more about Özgür and his work, please visit the</p>
-                            <a href="/contact" className="cta-button">Contact Page</a>
+                            <h4>Let's work together! Visit the <a href="#contact" className="custom-link" onClick={handleClick}>Contact Page</a> to connect and explore exciting opportunities.</h4>
                         </div>
                     </div>
                 </div>
