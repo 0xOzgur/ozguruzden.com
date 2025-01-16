f2e36ee2bd4c61bbdc70b3dc8e64f77b2bb2744a
0xOzgur <29779769+0xOzgur@users.noreply.github.com>
2025-01-17 00:09:21 +0300

    server config

--- cms/config/server.ts
+++ cms/config/server.ts
@@ -5,7 +5,7 @@ import fs from 'fs';
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env('PUBLIC_URL', 'https://ozguruzden.com/admin'),
+  url: env('PUBLIC_URL', 'https://ozguruzden.com:1337'),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET'),
