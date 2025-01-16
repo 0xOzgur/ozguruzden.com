45737430b4e0145e9b3334c86be79ec9800ae808
0xOzgur <29779769+0xOzgur@users.noreply.github.com>
2025-01-16 23:11:17 +0300

    middleware check

--- cms/config/middlewares.ts
+++ cms/config/middlewares.ts
@@ -18,11 +18,16 @@ export default [
       credentials: true,
     },
   },
-  'strapi::poweredBy',
-  'strapi::logger',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  module.exports = [
+    'strapi::errors',
+    'strapi::security',
+    'strapi::cors',
+    'strapi::poweredBy',
+    'strapi::logger',
+    'strapi::query',
+    'strapi::body',
+    'strapi::session',
+    'strapi::favicon',
+    'strapi::public',
+  ];
 ];
