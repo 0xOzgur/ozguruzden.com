4e7bfcc1bf026b551444a0a9dcc9034a9ba75db0
0xOzgur <29779769+0xOzgur@users.noreply.github.com>
2025-01-18 10:32:04 +0300

    back

--- cms/src/api/project/routes/project.ts
+++ cms/src/api/project/routes/project.ts
@@ -1,13 +1,7 @@
-module.exports = {
-    routes: [
-      {
-        method: 'GET',
-        path: '/projects',
-        handler: 'projects.find',
-        config: {
-          policies: [],
-        },
-      },
-      // ...
-    ],
-  };
+/**
+ * project router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::project.project');
