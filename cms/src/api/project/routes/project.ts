c5a8f886ae6977c0ee4a525171dea2d0e700b489
0xOzgur <29779769+0xOzgur@users.noreply.github.com>
2025-01-18 10:54:22 +0300

    routes fix

--- cms/src/api/project/routes/project.ts
+++ cms/src/api/project/routes/project.ts
@@ -1,18 +1,16 @@
-/**
- * project router
- */
-
 import { factories } from '@strapi/strapi';
 
 export default factories.createCoreRouter('api::project.project', {
-  routes: [
-    {
-      method: 'GET',
-      path: '/projects',
-      handler: 'projects.find',
-      config: {
-        policies: [],
+  config: {
+    routes: [
+      {
+        method: 'GET',
+        path: '/projects',
+        handler: 'projects.find',
+        config: {
+          policies: [],
+        },
       },
-    },
-  ],
+    ],
+  },
 });
